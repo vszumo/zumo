a43c94cd12ba1032b55c2a70da0f56f605f3d49c
lhridder <lhridder@gmail.com>
2023-06-26 21:43:09 +0200

    improve lijnvolger comments

--- Afstandbediening.cpp
+++ Afstandbediening.cpp
@@ -3,37 +3,35 @@
 
 #define snelheid 250
 
+// Afstandbediening() construeert de Afstandbediening en slaat de motorcontroller pointer op
 Afstandbediening::Afstandbediening(Motorcontroller* m):mc(m) {
   mc->zetBochtSnelheid(snelheid);
 }
 
+// koppel() start de seriele verbinding met de xbee ontvanger
 void Afstandbediening::koppel(){
-  Serial1.begin(9600); //de seriele poort te openen.
+  Serial1.begin(9600);
   Serial.println("Serial1 geopend");
 }
 
+// start() begint met het uitlezen
 void Afstandbediening::start(){
-  while(true) {
-    letter = Serial1.readString(); //Voor het inlezen van de input en deze input in de variabele letter stoppen.
 
-    if(letter.indexOf("w") == 0){ //Als de letter w ingevoerd word wordt het onderstaande uitgevoerd.
-      mc->rijdRecht(snelheid);
-    }
+  // bij het inlezen van de input slaan we deze op in de variabele letter
+  letter = Serial1.readString();
 
-    if(letter.indexOf("s") == 0){ //Als de letter s ingevoerd word wordt het onderstaande uitgevoerd.
-      mc->rijdRecht(-snelheid);
-    }
+  // als de letter w wordt ingevoerd rijd de robot vooruit met een constante snelheid
+  if(letter.indexOf("w") == 0) mc->rijdRecht(snelheid);
 
-    if(letter.indexOf("p") == 0){ //Als de letter p ingevoerd word wordt het onderstaande uitgevoerd.
-      mc->stop();
-    }
+  // als de letter s wordt ingevoerd rijd de robot achteruit met een constante snelheid
+  if(letter.indexOf("s") == 0) mc->rijdRecht(-snelheid);
 
-    if(letter.indexOf("a") == 0){ //Als de letter a ingevoerd word wordt het onderstaande uitgevoerd.
-      mc->maakBocht(0);
-    }
+  // als de letter p wordt ingevoerd stopt de robot de motoren
+  if(letter.indexOf("p") == 0) mc->stop();
 
-    if(letter.indexOf("d") == 0){ //Als de letter d ingevoerd word wordt het onderstaande uitgevoerd.
-      mc->maakBocht(1);
-    }
-  }
+  // als de letter a wordt ingevoerd maakt de robot een bocht naar links
+  if(letter.indexOf("a") == 0) mc->maakBocht(0);
+
+  // als de letter d wordt ingevoerd maakt de robot een bocht naar rechts
+  if(letter.indexOf("d") == 0) mc->maakBocht(1);
 }
